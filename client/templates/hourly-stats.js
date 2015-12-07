196b6115ac21cc500f0b946e0288ad28a53e82c1
Willy Chen <terepic@gmail.com>
2015-12-07 13:52:50 -0500

    Hourly counts done

--- client/templates/hourly-stats.js
+++ client/templates/hourly-stats.js
@@ -1,8 +1,25 @@
+Template.hourlyStats.helpers({
+	'hourlyCounts': function(){
+		var hourlyCounts = Session.get('hourlyStats');
+		hourResults = new Mongo.Collection(null);
+		console.log('counts');
+		for (i = 0; i <hourlyCounts.length; ++i){
+			//console.log(hourlyCounts[i].count);
+			console.log(hourlyCounts[i]._id.hour);
+
+			hourResults.insert({hour: hourlyCounts[i]._id.hour, count: hourlyCounts[i].count});
+			console.log(hourResults.findOne({hour: hourlyCounts[i]._id.hour}));
+		}		
+		return hourResults.find({}, {sort: {hour:1}});
+	}
+});
+
 Template.hourlyStats.events({
 	'click .getHourlyStats': function(){
 		console.log('Hourly Stats');
 		Meteor.call('getTweetsByHour', function(err, result){
 			Session.set('hourlyStats', result);
 		});
+		console.log(Session.get('hourlyStats'));
 	}
 });
--- server/aggregate-queries.js
+++ server/aggregate-queries.js
@@ -1,17 +1,8 @@
 userId = '17919972';
 Meteor.methods({
 	'getTweetsByHour': function(){
-	var pipeline = [
-		// {$match: {user_id: userId}},
-		{$group:{
-			_id:{ null}, //hour:{$hour: '$date'}},
-			// averageLength: {$avg: '$this.text.length'},
-			count: {$sum: 1}
-		 }
-		}
-	];
-	var result = Tweets.aggregate(pipeline);
-	console.log(results);
-	return result;		
+	var pipeline = [{$group:{_id: {hour: {$hour: '$date'}}, count: {$sum: 1}}}];
+	var hourlyCounts = Tweets.aggregate(pipeline);
+	return hourlyCounts;		
 	}
 });
