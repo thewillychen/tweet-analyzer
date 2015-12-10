55f375733e403c55374e8ba10db6bd0381a4f6ea
Lien <kimlienhg@gmail.com>
2015-12-10 09:25:04 -0500

    Deleted legacy file

--- client/templates/load-followers.js
+++
@@ -1,7 +0,0 @@
-Template.loadFollowers.events({
-  'click .loadUserFollowers': function () {
-    var username = 'taylorswift13';
-    console.log('Loading followers');
-    Meteor.call('loadUserFollowers', username);
-  }
-});
