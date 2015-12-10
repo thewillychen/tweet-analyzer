aed6a339fad0440a8683bda784008b650c96669f
Willy Chen <terepic@gmail.com>
2015-12-10 03:59:04 -0500

    Fixed goal input form

--- client/templates/daily-rate.js
+++ client/templates/daily-rate.js
@@ -10,7 +10,6 @@ Template.dailyRate.helpers({
 Template.dailyRate.onCreated(function(){
 	Meteor.call('getTweetRate', function(err, result){
 		Session.set('dailyRate', result);
-		console.log(Session.get('dailyRate'));
 	});		
 	return Session.get('dailyRate');
 });
